d53c9c2ad00c28a3dcd058e100924487b85e188a
Vincent Ogweno <ogwenovincent00@gmail.com>
2024-12-15 11:16:08 +0300

    Added error handling methods.
    Errors due to wrong user input can now be handled with try, throw and catch.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 //this repo initially had bugs and i came to find out the reason was the light mode i was using,
-//use darkmode mate itll help you
+//use darkmode mate it'll help you!
 //alter nothing, nimetransnight siku mzima debuggindg the bugs that i wrote
 
 //code crafted to nothing short of perfection
@@ -8,6 +8,7 @@
 
 #include "Bank.h"
 #include <iostream>
+#include <limits> // For numeric limits
 
 using namespace std;
 
@@ -15,42 +16,58 @@ int main() {
     int choice;
     char proceed;
     Bank obj;
+
     do {
-        cout << "Welcome to Fortis Bank!\n";
-        cout << "01) Open account\n";
-        cout << "02) Deposit money\n";
-        cout << "03) Withdraw money\n";
-        cout << "04) Fetch account info\n";
-        cout << "05) Exit\n";
-        cout << "Please select an option: ";
-        cin >> choice;
+        try {
+            cout << "Welcome to Fortis Bank!\n";
+            cout << "01) Open account\n";
+            cout << "02) Deposit money\n";
+            cout << "03) Withdraw money\n";
+            cout << "04) Fetch account info\n";
+            cout << "05) Exit\n";
+            cout << "Please select an option: ";
 
-        switch (choice) {
-            case 1:
-                obj.open_account();
-                break;
-            case 2:
-                obj.deposit_money();
-                break;
-            case 3:
-                obj.withdraw_money();
-                break;
-            case 4: {
-                long long account_number;
-                cout << "Enter your account number: ";
-                cin >> account_number;
-                obj.fetch_account_info(account_number);
-                break;
+            if (!(cin >> choice)) {
+                throw invalid_argument("Invalid input! Please enter a number between 1 and 5.");
             }
-            case 5:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid option.\n";
-        }
 
-        cout << "\nDo you want to select another option? (y/n): ";
-        cin >> proceed;
+            switch (choice) {
+                case 1:
+                    obj.open_account();
+                    break;
+                case 2:
+                    obj.deposit_money();
+                    break;
+                case 3:
+                    obj.withdraw_money();
+                    break;
+                case 4: {
+                    long long account_number;
+                    cout << "Enter your account number: ";
+                    if (!(cin >> account_number)) {
+                        cin.clear(); // Clear the error flag
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
+                        throw invalid_argument("Invalid input! Account number must be numeric.");
+                    }
+                    obj.fetch_account_info(account_number);
+                    break;
+                }
+                case 5:
+                    cout << "Exiting...\n";
+                    break;
+                default:
+                    throw out_of_range("Invalid option! Please select a number between 1 and 5.");
+            }
+
+            cout << "\nDo you want to select another option? (y/n): ";
+            cin >> proceed;
+
+        } catch (const exception &e) {
+            cout << "Error: " << e.what() << endl;
+            proceed = 'y'; // Allow user to retry after an error
+            cin.clear(); // Clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Ignore invalid input
+        }
 
     } while (proceed == 'y' || proceed == 'Y');
 
